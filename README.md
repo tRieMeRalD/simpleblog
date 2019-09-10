64e1bbc327a11f4ef7bb8c189136eb0d005d6a2a
Will L <horizon.william14@gmail.com>
2019-09-09 22:00:11 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Simple Blog
+
+Ruby on Rails
+
+## Update threads
+
+### September 9, 2019 Update
+
+- Initial Commit
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'posts#index', as: "home"
+  get 'about' -> 'pages#about', as: "about"
+
+  resources :posts do
+    resources :comments
+  end
+end
